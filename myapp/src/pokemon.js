3a9a263a9890f374c5765031fc7d84a701bbda2c
puumbaaa <78651716+puumbaaa@users.noreply.github.com>
2022-12-13 16:40:05 +0100

    Update pokemon.js

--- myapp/src/pokemon.js
+++ myapp/src/pokemon.js
@@ -52,3 +52,16 @@ export const deletePokemon = async (pokemon) => {
     const pokemons = await response.json()
     return pokemons
 }
+export const updatePokemon = async (pokemon) => {
+    const response = await fetch(
+        'http://localhost:4444/pokemon/update', {
+            method: 'POST', 
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(pokemon)
+        }
+    )
+    .then (response => response.json())
+}
